4d43149a48602ce2fbf9c9da8d452a9eff7d3a60
jang <jdj81204@naver.com>
2025-02-08 19:11:52 +0900

    first test commit

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>{"아오 시발 진짜"}</div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test/java/com/personal/doit/DoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.doit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
